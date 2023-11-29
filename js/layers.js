05a89dcce4162bb99f8931a138c8e2e10de71004
Thien-BPS <ttran113@bostonk12.org>
2023-11-29 11:08:48 -0500

    test, upgrade class

--- js/layers.js
+++ js/layers.js
@@ -1,61 +1,177 @@
 import Decimal from "./technical/break_eternity.js"
-import { player } from "./game.js"
-import * as otherUtils from "./utils/easyAccess.js"
-import Utils from "./utils.js"
-import { addLayer } from "./technical/layerSupport.js"
 
-export class baseLayerState {
-    get layer() {
-        throw new Error("Define it in layers.")
+function NotImplementedError(message = "") {
+    this.name = "NotImplementedError";
+    this.message = message;
+  }
+NotImplementedError.prototype = Error.prototype;
+
+function isFunction(obj) {
+    return !!(obj && obj.constructor && obj.call && obj.apply);
+}
+
+export class UpgradeMechanicState {
+    constructor(confg) {
+        this._config = confg;
     }
 
-    get name() {
-        throw new Error("Define it in layers.")
-    }
-    
-    get symbol() {
-        throw new Error("Define it in layers.")
+    get cost() {
+        throw new NotImplementedError("This should be overrided with layer features.");
     }
 
-    get position() {
-        return 0;
+    get effect() {
+        throw new NotImplementedError("This should be overrided with layer features.");
     }
 
-    get startData() {
-        return layers[this.layer].startData
+    get description() {
+        throw new NotImplementedError("This should be overrided with layer features.");
     }
 
-    set startData() {
-        if ()
-        layers[this.layer].startData = 
+    get title() {
+        throw new NotImplementedError("This should be overrided with layer features.");
+    }
+
+    get unlocked() {
+        throw new NotImplementedError("This should be overrided with layer features.");
+    }
+
+    get effectDisplay() {
+        throw new NotImplementedError("This should be overrided with layer features.");
+    }
+
+    get formula() {
+        throw new NotImplementedError("This should be overrided with layer features.");
+    }
+
+    get canAfford() {
+        throw new NotImplementedError("This should be overrided with layer features.");
     }
 }
 
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
-    }},
-    color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
-    },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (Utils.canReset(this.layer)) Utils.doReset(this.layer)}},
-    ],
-    layerShown(){return true}
-})
+export class ExampleUpgradeState extends UpgradeMechanicState {
+    get costExpBase() {
+        return this._config.costExponentBase;
+    }
+
+    get costExp() {
+        return this._config.costExponent;
+    }
+
+    get costBase() {
+        return this._config.minimumCost;
+    }
+
+    get cost() {
+        return Decimal.pow(this.costExpBase, this.costExp).add(this.costBase);
+    }
+
+    get effectType() {
+        return this._config.effectType;
+    }
+
+    get effectSubType() {
+        return this._config.effectSubType
+    }
+
+    effectParams(param, ...args) {
+        const calling = this._config.effectObject[param];
+        return isFunction(calling) ? calling(args) : calling;
+    }
+
+    get effect() {
+        const type = this.effectType;
+        const subType = this.effectSubType
+        let firstEff;
+        switch (type) {
+            case "linear":
+                firstEff = Decimal.times(this.effectParams("base"), this.effectParams("multiplier"));
+                switch (subType) {
+                    case "noOtherBoosts":
+                        return firstEff;
+                    case "onlyAdders":
+                        return firstEff.add(this.effectParams("effectAdders"));
+                    case "onlyMults":
+                        return firstEff.times(this.effectParams("effectMults"));
+                    case "onlyPow":
+                        return firstEff.max(1).pow(this.effectParams("effectPowers"));
+                    case "":
+                        return this.effectParams("handleBaseEffect", firstEff, 1);
+                    case "effectAndSoftcaps":
+                        return this.effectParams("handleBaseEffect", firstEff, 1);
+                    case "onlySoftcaps":
+                        return this.effectParams("handleBaseEffect", firstEff, 2);
+                    case "onlyEffect":
+                        return this.effectParams("handleBaseEffect", firstEff, 3);
+                }
+            case "exponentional":
+                firstEff = Decimal.pow(this.effectParams("expBase"), this.effectParams("exp")).max(this.effectParams("minimumEffValue"));
+                switch (subType) {
+                    case "noOtherBoosts":
+                        return firstEff;
+                    case "onlyAdders":
+                        return firstEff.add(this.effectParams("effectAdders"));
+                    case "onlyMults":
+                        return firstEff.times(this.effectParams("effectMults"));
+                    case "onlyPow":
+                        return firstEff.max(1).pow(this.effectParams("effectPowers"));
+                    case "onlyLogarithm":
+                        return firstEff.max(1).log(this.effectParams("effectLogarithmBase"));
+                    case "onlyRoot":
+                        return firstEff.root(this.effectParams("rootPower"));
+                    case "":
+                        return this.effectParams("handleBaseEffect", firstEff, 1);
+                    case "effectAndSoftcaps":
+                        return this.effectParams("handleBaseEffect", firstEff, 1);
+                    case "onlySoftcaps":
+                        return this.effectParams("handleBaseEffect", firstEff, 2);
+                    case "onlyEffect":
+                        return this.effectParams("handleBaseEffect", firstEff, 3);
+                }
+            case "multiExp" || "tetrational":
+                firstEff = Decimal.tetrate(this.effectParams("tetrateAmount"), this.effectParams("tetrateBase"), false).max(this.effectParams("minimumEffValue"));
+                switch (subType) {
+                    case "noOtherBoosts":
+                        return firstEff;
+                    case "onlyPow":
+                        return firstEff.max(1).pow(this.effectParams("effectPowers"));
+                    case "onlyLogarithm":
+                        return firstEff.max(1).log(this.effectParams("effectLogarithmBase"));
+                    case "onlyRoot":
+                        return firstEff.root(this.effectParams("rootPower"));
+                    case "onlyRepeatedLog":
+                        return firstEff.max(1).iteratedlog(this.effectParams("effectLogarithmBase"), this.effectParams("effectLogRepeat"), false);
+                    case "onlyRepeatedPower":
+                        return firstEff.max(1).iteratedexp(this.effectParams("effectPowers"), this.effectParams("effectPowerBase"), false);
+                    case "onlySuperLogarithm":
+                        return firstEff.max(1).slog(this.effectParams("effectLogarithmBase"));
+                    case "":
+                        return this.effectParams("handleBaseEffect", firstEff, 1);
+                    case "effectAndSoftcaps":
+                        return this.effectParams("handleBaseEffect", firstEff, 1);
+                    case "onlySoftcaps":
+                        return this.effectParams("handleBaseEffect", firstEff, 2);
+                    case "onlyEffect":
+                        return this.effectParams("handleBaseEffect", firstEff, 3);
+                }
+            case "pentational":
+                // really dangerous, can reach infinity very quickly.
+                firstEff = Decimal.pentate(this.effectParams("pentateBase"), this.effectParams("pentateAmount"), false).max(this.effectParams("minimumEffValue"));
+                switch (subType) {
+                    case "":
+                        return this.effectParams("handleBaseEffect", firstEff);
+                    case "effectAndSoftcaps":
+                        return this.effectParams("handleBaseEffect", firstEff, true);
+                    case "onlySoftcaps":
+                        return this.effectParams("handleBaseEffect", firstEff, false);
+                }
+            case "custom":
+                return this.effectParams("handleBaseEffect", this.effectParams("baseEffect"), 1)
+            default:
+                throw new TypeError(`Unknown Effect Type: ${type}`);
+        }
+    }
+
+    get description() {
+        return new Decimal.pow(this.costExpBase, this.costExp).add(this.costBase);
+    }
+}
